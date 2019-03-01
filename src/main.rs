a6c8d7a0082e3b4ead2dab60635541a4ce62e8df
zvkemp <zvkemp@gmail.com>
2019-03-01 14:55:36 -0800

    wip toml config

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,11 @@
 // - separate config/template repository; maintain local checkout from git
 //
 fn main() {
-    let mut template = Template::new(
-            "templates/test.handlebars",
+    let mut template = Template::new_from_file(
+            "templates/test/hello.hbs",
             "test/test.conf",
-            HostConfig { username: String::from("zach") }
+            HostConfig { username: String::from("zach") },
+            None,
         );
 
     println!("{}", template.render());
@@ -22,12 +23,16 @@ fn main() {
 
 use std::path::Path;
 use handlebars::to_json;
+use toml;
+
+type Custom = Option<toml::Value>;
 
 #[derive(Debug)]
 pub struct Template {
     host_config: HostConfig,
     template_string: String,
     target_path: String,
+    custom: Custom,
 }
 
 // TODO:
@@ -37,8 +42,22 @@ pub struct HostConfig {
     username: String,
 }
 
+mod util {
+    pub fn whoami<'a>() -> String {
+        let stdout = std::process::Command::new("whoami").output().expect("tried to get username").stdout;
+        std::str::from_utf8(&stdout).expect("").trim().into()
+    }
+}
+
+impl HostConfig {
+    pub fn default() -> Self {
+        HostConfig { username: util::whoami() }
+    }
+}
+
+
 impl Template {
-    pub fn new(template_path: &str, target_path: &str, host_config: HostConfig) -> Self {
+    pub fn new_from_file(template_path: &str, target_path: &str, host_config: HostConfig, custom: Custom) -> Self {
         fn read_template(path: &str) -> std::io::Result<String> {
             use std::fs::File;
             use std::io::prelude::*;
@@ -51,10 +70,15 @@ impl Template {
             Ok(contents)
         }
 
+        Self::new(&read_template(template_path).unwrap(), target_path, host_config, custom)
+    }
+
+    pub fn new(template_string: &str, target_path: &str, host_config: HostConfig, custom: Custom) -> Self {
         Template {
             host_config,
             target_path: String::from(target_path),
-            template_string: read_template(template_path).unwrap(),
+            template_string: String::from(template_string),
+            custom
         }
     }
 
@@ -85,9 +109,40 @@ impl Serialize for Template {
         S: Serializer,
     {
         // TODO: add 'platform' to top level
-        let mut s = serializer.serialize_struct("Template", 2)?;
+        let mut s = serializer.serialize_struct("Template", 3)?;
         s.serialize_field("host_config", &self.host_config)?;
         s.serialize_field("target_path", &self.target_path)?;
+        s.serialize_field("custom", &self.custom)?;
         s.end()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_rendering_template() {
+        let tmpl = Template::new_from_file(
+            "templates/test/hello.hbs",
+            "target/test_out/hello.conf",
+            HostConfig::default(),
+            None,
+        );
+
+        assert_eq!(tmpl.render(), format!("hello {}\n", util::whoami()));
+    }
+
+    #[test]
+    fn test_rendering_template_with_custom_values() {
+        let custom: Custom = "[alternate_greeting]\nen = \"greetings!\"".parse::<toml::Value>().ok();
+        let tmpl = Template::new_from_file(
+            "templates/test/hello.hbs",
+            "target/test_out/hello.conf",
+            HostConfig::default(),
+            custom
+        );
+
+        assert_eq!(tmpl.render(), format!("hello {}\ngreetings!\n", util::whoami()));
     }
 }
